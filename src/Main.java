0b3975d48f41143793646e8cfb1fda8c07fbf709
Yana <bondarevayana@gmail.com>
2022-10-03 12:13:36 +0300

    Lambda added instead of Comparator class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -15,12 +14,20 @@ public class Main {
         Person person6 = new Person("Эдвард", "Артуа Феникс Гиннес Айви", 45);
         Person person7 = new Person("Дуэйн", "Ричард Гиннес Айви Уолден Младший", 85);
         List<Person> people = new ArrayList<>(Arrays.asList(person1, person2, person3, person4, person5, person6, person7));
-        Collections.sort(people, new SurnameComparator());
+        people.sort ((Person o1, Person o2)-> {if (o1.wordsQuantity() < o2.wordsQuantity()) {
+            return -1;
+        } else if (o1.wordsQuantity() > o2.wordsQuantity()) {
+            return 1;
+        } else
+        {return Integer.compare(o1.getAge(), o2.getAge());}
+        });
+
 
         for (int i = 0; i < people.size(); i++) System.out.println(people.get(i).toSting());
     }
-
-
 }
+
+
+
 
 
